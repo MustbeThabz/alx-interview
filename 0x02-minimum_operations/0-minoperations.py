efd4e574a7fbac0a34518f21da8a93be9a9f1511
Tebogo <tebogodipalett@gmail.com>
2023-08-10 23:40:37 +0000

    bug fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+import math
+
+
+def factors(n):
+    """factors of n number"""
+    mylist = []
+    while n % 2 == 0:
+        mylist.append(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            mylist.append(i)
+            n = n / i
+    if n > 2:
+        mylist.append(n)
+    return mylist
+
 
 def minOperations(n):
-    if n <= 1:
-        return n
-
-    operations = [float('inf')] * (n + 1)
-    operations[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + (i // j))
-
-    return operations[n]
+    """calculate the minimum operations"""
+    if type(n) != int or n < 2:
+        return 0
+    else:
+        numOperations = sum(factors(n))
+        return int(numOperations)
